2ebd34a50cc875357928f4a63273d8bc2247269d
Alexander Cumberworth <alexandercumberworth@gmail.com>
2016-11-04 16:56:21 +0000

    Fixed endpoint bug in cyclic systems

--- scripts/sequtils/calc_average_melting_point.py
+++ scripts/sequtils/calc_average_melting_point.py
@@ -5,9 +5,9 @@
 import argparse
 import sys
 
-sys.path.insert(0, '../../lib/lattice_origami_domains')
+sys.path.insert(0, '../lattice_origami_domains')
 
-from lattice_dna_origami.lattice_origami_domains import *
+from lattice_dna_origami.origami_io import *
 from lattice_dna_origami.nearest_neighbour import *
 
 parser = argparse.ArgumentParser()
@@ -22,9 +22,9 @@ cation_M = args.cation_M
 
 input_file = JSONInputFile(config_filename)
 
-# Calculate all melting points
+# Calculate melting points of individual fully bound domains
 melting_points = []
-for seq in input_file.sequences:
+for seq in input_file.sequences[0]:
     melting_point = calc_melting_point(seq, strand_M, cation_M)
     melting_points.append(melting_point)
 
@@ -33,7 +33,7 @@ mean_T = np.mean(melting_points)
 min_T = min(melting_points)
 max_T = max(melting_points)
 
-print('Unbound melting temperatures:')
+print('Isolated domain melting temperatures:')
 print('Average: {:.1f} K'.format(mean_T))
 print('Maximum: {:.1f} K'.format(max_T))
 print('Minimum: {:.1f} K'.format(min_T))
@@ -41,7 +41,7 @@ print()
 
 # Calculate internal melting temperatures
 internal_melting_points = []
-for seq in input_file.sequences:
+for seq in input_file.sequences[0]:
     internal_melting_point = calc_internal_melting_point(seq, cation_M)
     internal_melting_points.append(internal_melting_point)
 
@@ -50,7 +50,25 @@ mean_iT = np.mean(internal_melting_points)
 min_iT = min(internal_melting_points)
 max_iT = max(internal_melting_points)
 
-print('Bound melting temperatures:')
+print('Bound staple domain melting temperatures:')
 print('Average: {:.1f} K'.format(mean_iT))
 print('Maximum: {:.1f} K'.format(max_iT))
 print('Minimum: {:.1f} K'.format(min_iT))
+print()
+
+# Calculate melting points of fully bound staples
+staple_melting_points = []
+for staple in input_file.sequences[1:]:
+    staple_seq = ''.join(staple)
+    staple_melting_point = calc_melting_point(staple_seq, strand_M, cation_M)
+    staple_melting_points.append(staple_melting_point)
+
+mean_sT = np.mean(staple_melting_points)
+min_sT = min(staple_melting_points)
+max_sT = max(staple_melting_points)
+
+print('Whole staple melting temperatures:')
+print('Average: {:.1f} K'.format(mean_sT))
+print('Maximum: {:.1f} K'.format(max_sT))
+print('Minimum: {:.1f} K'.format(min_sT))
+print()
