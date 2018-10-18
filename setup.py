21c06f3092a4c687fe2908678e9f17ee1db6d6cb
Alexander <alexandercumberworth@gmail.com>
2018-10-18 14:43:08 +0100

    Created setup script for python stuff.

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup script for analysis module and related scripts LatticeDNAOrigami"""
+
+import glob
+from setuptools import setup
+
+setup(
+    name='origamipy',
+    packages=['origamipy'],
+    scripts=glob.glob('scripts/analysis/*.py')
+)
